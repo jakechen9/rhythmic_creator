da842572efb41697c87d422ffa5a03aa5e5d6a06
jakechen9 <jakecheng@alum.calarts.edu>
2024-03-12 18:18:30 -0700

    add train

--- models/train.py
+++ models/train.py
@@ -1,72 +1,40 @@
 import torch
 from preprocess.batch import Batch
-from preprocess.preprocessing import MIDIProcessor
-from models.transformerdecoder import DecoderModel
-
-torch.manual_seed(1337)
-
-path = '/content/rhythmic_creator/training_1.txt'
-MIDIPreprocess = MIDIProcessor(path)
-midi_flatten, unique_notes = MIDIPreprocess.split_text()
-data = torch.tensor(MIDIPreprocess.encode_with_mapping(midi_flatten), dtype=torch.long)
-
-# input size
-batch_size = 64  # b
-block_size = 256  # t
-vocab_size = len(unique_notes)  # c
-
-# attention mechanism params
-n_embd = 192
-num_heads = 6
-n_layer = 6
-
-# optimizer params
-dropout = 0.2
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# eval params
-max_iters = 5000
-evaluation_interval = 500
-evaluation_iters = 200
-
-get_batch = Batch(data, block_size, batch_size)
 
 
-# model = DecoderModel(block_size, vocab_size, n_embd, num_heads, n_layer, dropout)
-# m = model.to(device)
-#
-# print(sum(p.numel() for p in m.parameters()) / 1e6, 'M parameters')
-#
-# optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+class Train(Batch):
+    def __init__(self, model, optimizer, evaluation_iters, indata, block_size, batch_size):
+        super().__init__(indata, block_size, batch_size)
+        self.evaluation_iters = evaluation_iters
+        self.model = model
+        self.optimizer = optimizer
 
-@torch.no_grad()
-def estimate_loss(model):
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(evaluation_iters)
-        for k in range(evaluation_iters):
-            X, Y = get_batch.create_batch(split, device)
-            logits, loss = model(device, X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
+    @torch.no_grad()
+    def estimate_loss(self, device):
+        out = {}
+        self.model.eval()
+        for split in ['train', 'val']:
+            losses = torch.zeros(self.evaluation_iters)
+            for k in range(self.evaluation_iters):
+                X, Y = self.create_batch(split, device)
+                logits, loss = self.model(device, X, Y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        self.model.train()
+        return out
 
+    def train(self, max_iters, evaluation_interval, device):
+        for iteration in range(max_iters):
+            # every once in a while evaluate the loss on train and val sets
+            if iteration % evaluation_interval == 0 or iteration == max_iters - 1:
+                losses = self.estimate_loss(self.model, device)
+                print(f"step {iteration}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-def train(model, optimizer):
-    for iteration in range(max_iters):
-        # every once in a while evaluate the loss on train and val sets
-        if iteration % evaluation_interval == 0 or iteration == max_iters - 1:
-            losses = estimate_loss(model)
-            print(f"step {iteration}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            # sample a batch of data
+            xb, yb = self.create_batch('train', device)
 
-        # sample a batch of data
-        xb, yb = get_batch.create_batch('train', device)
-
-        # evaluate the loss
-        logits, loss = model(device, xb, yb)
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
+            # evaluate the loss
+            logits, loss = self.model(device, xb, yb)
+            self.optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            self.optimizer.step()
