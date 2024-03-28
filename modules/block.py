0089d3927da49a3601e0d5a228de6129a9fd6693
jakechen9 <jakecheng@alum.calarts.edu>
2024-03-28 15:04:48 -0700

    adjust lstm input

--- modules/block.py
+++ modules/block.py
@@ -34,9 +34,8 @@ class BlockTwo(nn.Module):
 
     def forward(self, inputs):
         x, hidden = inputs
-        x_0, h_0 = self.lstm_lyr(self.lyr_norm4(x), hidden)
-        x, h = x + x_0, hidden + h_0
         x = x + self.sa(self.lyr_norm1(x))
+        x, h = self.lstm_lyr(self.lyr_norm4(x), hidden)
         x = x + self.ffwd(self.lyr_norm2(x))  # skip connection, residual connection.
         # x = x + self.sa(self.lyr_norm4(x))
         return x, h
