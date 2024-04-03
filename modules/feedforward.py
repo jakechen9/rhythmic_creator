11200f2fc9213b505b355c3f4d3a67600dcd888a
jakechen9 <jakecheng@alum.calarts.edu>
2024-04-03 09:47:31 -0700

    add init weight for lstm

--- modules/feedforward.py
+++ modules/feedforward.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
-
-
+import torch
+import math
 class MlPFeedForward(nn.Module):
     def __init__(self, n_embd, dropout):
         super().__init__()
@@ -36,9 +36,24 @@ class LSTMFeedForward(nn.Module):
                             batch_first=True)
         self.dl = nn.Dropout(dropout)
         self.fc = nn.Linear(n_hidden, n_embd)
+        self.lstm_layers = lstm_layers
+        self.n_embd = n_embd
+        self.n_hidden = n_hidden
+        self.init_weights()
 
     def forward(self, x, hidden):
         x, h = self.lstm(x, hidden)
         x = self.dl(x)
         x = self.fc(x)
         return x, h
+
+    def _init_weights(self):
+        non_embd_layer_range = 1 / math.sqrt(self.n_hidden)
+        for i in range(self.lstm_layers):
+            self.lstm.all_weights[i][0] = torch.FloatTensor(self.n_embd, self.n_hidden).uniform_(-non_embd_layer_range,
+                                                                                                 non_embd_layer_range)
+            self.lstm.all_weights[i][1] = torch.FloatTensor(self.n_hidden, self.n_hidden).uniform_(
+                -non_embd_layer_range, non_embd_layer_range)
+
+
+
